7f9fcc2bf03be3a8d7d83768590adb0a02e5c27b
Tobin Feldman-Fitzthum <tobin@ibm.com>
2022-11-15 16:36:40 +0000

    WIP: Add simple SNP attester
    Signed-off-by: Tobin Feldman-Fitzthum <tobin@ibm.com>

--- src/kbc_modules/cc_kbc/attester/mod.rs
+++ src/kbc_modules/cc_kbc/attester/mod.rs
@@ -10,17 +10,20 @@ pub mod sample;
 #[cfg(feature = "tdx-attester")]
 pub mod tdx;
 
+#[cfg(feature = "snp-attester")]
+pub mod snp;
+
 /// The supported TEE types:
 /// - Tdx: TDX TEE.
 /// - Sgx: SGX TEE.
-/// - Sevsnp: SEV-SNP TEE.
+/// - Snp: SEV-SNP TEE.
 /// - Sample: A dummy TEE that used to test/demo the KBC functionalities.
 #[derive(Debug, EnumString, Display)]
 #[strum(ascii_case_insensitive, serialize_all = "lowercase")]
 pub enum Tee {
     Tdx,
     Sgx,
-    Sevsnp,
+    Snp,
     Sample,
     Unknown,
 }
@@ -31,6 +34,8 @@ impl Tee {
             Tee::Sample => Ok(Box::<sample::SampleAttester>::default()),
             #[cfg(feature = "tdx-attester")]
             Tee::Tdx => Ok(Box::<tdx::TdxAttester>::default()),
+            #[cfg(feature = "snp-attester")]
+            Tee::Snp => Ok(Box::<snp::SnpAttester>::default()),
             _ => bail!("TEE is not supported!"),
         }
     }
@@ -49,5 +54,9 @@ pub fn detect_tee_type() -> Tee {
     if tdx::detect_platform() {
         return Tee::Tdx;
     }
+    #[cfg(feature = "snp-attester")]
+    if snp::detect_platform() {
+        return Tee::Snp;
+    } 
     Tee::Unknown
 }
